c47082a264c3ea11a516fd62ddca1eeb746365cd
pigstd <zhuzemu@sjtu.edu.cn>
2025-09-19 13:48:51 +0800

    添加了 unit type, 完成除了解析表达式以外的所有语法解析

--- src/ast.cpp
+++ src/ast.cpp
@@ -33,6 +33,7 @@ void ItemStmt::accept(AST_visitor &v) { v.visit(*this); }
 
 void PathType::accept(AST_visitor &v) { v.visit(*this); }
 void ArrayType::accept(AST_visitor &v) { v.visit(*this); }
+void UnitType::accept(AST_visitor &v) { v.visit(*this); }
 
 void IdentifierPattern::accept(AST_visitor &v) { v.visit(*this); }
 
@@ -138,6 +139,7 @@ void AST_Walker::visit(ArrayType &node) {
     node.element_type->accept(*this);
     node.size_expr->accept(*this);
 }
+void AST_Walker::visit([[maybe_unused]] UnitType &node) { return; }
 void AST_Walker::visit([[maybe_unused]] IdentifierPattern &node) { return; }
 
 vector<Item_ptr> Parser::parse() {
@@ -277,4 +279,213 @@ ConstItem_ptr Parser::parse_const_item() {
     Expr_ptr value = parse_expression();
     lexer.consume_expect_token(Token_type::SEMICOLON);
     return std::make_unique<ConstItem>(const_name, std::move(const_type), std::move(value));
+}
+
+Pattern_ptr Parser::parse_pattern() {
+    // example :
+    // identifier, mut identifier, &mut identifier, &identifier
+    ReferenceType ref_type = ReferenceType::NO_REF;
+    if (lexer.peek_token().type == Token_type::AMPERSAND) {
+        lexer.consume_expect_token(Token_type::AMPERSAND);
+        ref_type = ReferenceType::REF;
+    }
+    Mutibility mut_type = Mutibility::IMMUTABLE;
+    if (lexer.peek_token().type == Token_type::MUT) {
+        lexer.consume_expect_token(Token_type::MUT);
+        mut_type = Mutibility::MUTABLE;
+    }
+    string identifier_name = lexer.consume_expect_token(Token_type::IDENTIFIER).value;
+    return std::make_unique<IdentifierPattern>(identifier_name, mut_type, ref_type);
+}
+
+LetStmt_ptr Parser::parse_let_statement() {
+    // example :
+    // let pattern : Type (= initializer);
+    // 可以没有定义
+    lexer.consume_expect_token(Token_type::LET);
+    Pattern_ptr pattern = parse_pattern();
+    lexer.consume_expect_token(Token_type::COLON);
+    Type_ptr type = parse_type();
+    Expr_ptr initializer = nullptr;
+    if (lexer.peek_token().type == Token_type::EQUAL) {
+        lexer.consume_expect_token(Token_type::EQUAL);
+        initializer = parse_expression();
+    }
+    lexer.consume_expect_token(Token_type::SEMICOLON);
+    return std::make_unique<LetStmt>(std::move(pattern), std::move(initializer));
+}
+ExprStmt_ptr Parser::parse_expr_statement() {
+    // example :
+    // expression;
+    Expr_ptr expr = parse_expression();
+    bool is_semi = false;
+    if (lexer.peek_token().type == Token_type::SEMICOLON) {
+        lexer.consume_expect_token(Token_type::SEMICOLON);
+        is_semi = true;
+    }
+    return std::make_unique<ExprStmt>(std::move(expr), is_semi);
+}
+ItemStmt_ptr Parser::parse_item_statement() {
+    // example :
+    // item
+    Item_ptr item = parse_item();
+    return std::make_unique<ItemStmt>(std::move(item));
+}
+
+Stmt_ptr Parser::parse_statement() {
+    // 这里只需要考虑 let, expression, item
+    Token token = lexer.peek_token();
+    if (token.type == Token_type::LET) {
+        return parse_let_statement();
+    } else if (token.type == Token_type::FN ||
+               token.type == Token_type::STRUCT ||
+               token.type == Token_type::ENUM ||
+               token.type == Token_type::IMPL ||
+               token.type == Token_type::CONST) {
+        return parse_item_statement();
+    } else {
+        return parse_expr_statement();
+    }
+}
+
+BlockExpr_ptr Parser::parse_block_expression() {
+    // example :
+    // { statement1; statement2; ... tail_statement }
+    lexer.consume_expect_token(Token_type::LEFT_BRACE);
+    vector<Stmt_ptr> statements;
+    Stmt_ptr tail_statement = nullptr;
+    bool tail_statement_has_semi = true;
+    while (lexer.peek_token().type != Token_type::RIGHT_BRACE) {
+        // 跳过多余的分号
+        if (lexer.peek_token().type == Token_type::SEMICOLON) {
+            lexer.consume_expect_token(Token_type::SEMICOLON);
+            continue;
+        }
+        // 只有 expression 需要考虑有没有分号
+        bool has_semi = true;
+        Stmt_ptr stmt = parse_statement();
+        if (auto expr_stmt = dynamic_cast<ExprStmt*>(stmt.get())) {
+            if (!expr_stmt->is_semi) { has_semi = false; }
+        }
+        if (tail_statement) {
+            if (!tail_statement_has_semi) {
+                // 如果是 if, loop，那么打上标记，返回值一定是 ()
+                // 如果 While，不用管
+                // 否则 CE
+                if (auto if_expr = dynamic_cast<IfExpr*>(tail_statement.get())) {
+                    if_expr->must_return_unit = true;
+                } else if (auto loop_expr = dynamic_cast<LoopExpr*>(tail_statement.get())) {
+                    loop_expr->must_return_unit = true;
+                } else if (auto while_expr = dynamic_cast<WhileExpr*>(tail_statement.get())) {
+                    // do nothing
+                    // while 一定返回 ()
+                } else {
+                    throw string("CE, unexpected statement after expression without semicolon");
+                }
+            }
+            statements.push_back(std::move(tail_statement));
+        }
+        tail_statement = std::move(stmt);
+        tail_statement_has_semi = has_semi;
+    }
+    lexer.consume_expect_token(Token_type::RIGHT_BRACE);
+    if (tail_statement_has_semi) {
+        if (tail_statement) {
+            statements.push_back(std::move(tail_statement));
+        }
+        tail_statement = nullptr;
+        return std::make_unique<BlockExpr>(std::move(statements), nullptr);
+    }
+    else {
+        return std::make_unique<BlockExpr>(std::move(statements), std::move(tail_statement));
+    }
+}
+
+
+IfExpr_ptr Parser::parse_if_expression() {
+    // example :
+    // if (condition) { then_branch } (else { else_branch })?
+    // 有一个特殊情况： else 后面如果跟的是 if 就不用大括号
+    lexer.consume_expect_token(Token_type::IF);
+    lexer.consume_expect_token(Token_type::LEFT_PARENTHESIS);
+    Expr_ptr condition = parse_expression();
+    lexer.consume_expect_token(Token_type::RIGHT_PARENTHESIS);
+    Expr_ptr then_branch = parse_block_expression();
+    Expr_ptr else_branch = nullptr;
+    if (lexer.peek_token().type == Token_type::ELSE) {
+        lexer.consume_expect_token(Token_type::ELSE);
+        // else 可能后面跟 if 也可能跟 {
+        if (lexer.peek_token().type == Token_type::IF) { else_branch = parse_if_expression(); }
+        else  {else_branch = parse_block_expression(); }
+    }
+    return std::make_unique<IfExpr>(std::move(condition), std::move(then_branch), std::move(else_branch));
+}
+WhileExpr_ptr Parser::parse_while_expression() {
+    // example :
+    // while (condition) { body }
+    lexer.consume_expect_token(Token_type::WHILE);
+    lexer.consume_expect_token(Token_type::LEFT_PARENTHESIS);
+    Expr_ptr condition = parse_expression();
+    lexer.consume_expect_token(Token_type::RIGHT_PARENTHESIS);
+    Expr_ptr body = parse_block_expression();
+    return std::make_unique<WhileExpr>(std::move(condition), std::move(body));
+}
+LoopExpr_ptr Parser::parse_loop_expression() {
+    // example :
+    // loop { body }
+    lexer.consume_expect_token(Token_type::LOOP);
+    Expr_ptr body = parse_block_expression();
+    return std::make_unique<LoopExpr>(std::move(body));
+}
+Type_ptr Parser::parse_type() {
+    // example :
+    // identifier, [Type; size_expr], (), &Type, &mut Type
+    ReferenceType ref_type = ReferenceType::NO_REF;
+    if (lexer.peek_token().type == Token_type::AMPERSAND) {
+        lexer.consume_expect_token(Token_type::AMPERSAND);
+        ref_type = ReferenceType::REF;
+    }
+    Mutibility mut_type = Mutibility::IMMUTABLE;
+    if (lexer.peek_token().type == Token_type::MUT) {
+        lexer.consume_expect_token(Token_type::MUT);
+        mut_type = Mutibility::MUTABLE;
+    }
+    if (lexer.peek_token().type == Token_type::LEFT_BRACKET) {
+        // [Type, size_expr]
+        lexer.consume_expect_token(Token_type::LEFT_BRACKET);
+        Type_ptr element_type = parse_type();
+        lexer.consume_expect_token(Token_type::SEMICOLON);
+        Expr_ptr size_expr = parse_expression();
+        lexer.consume_expect_token(Token_type::RIGHT_BRACKET);
+        return std::make_unique<ArrayType>(std::move(element_type), std::move(size_expr), mut_type, ref_type);
+    }
+    else if (lexer.peek_token().type == Token_type::LEFT_PARENTHESIS) {
+        // ()
+        lexer.consume_expect_token(Token_type::LEFT_PARENTHESIS);
+        lexer.consume_expect_token(Token_type::RIGHT_PARENTHESIS);
+        return std::make_unique<UnitType>(mut_type, ref_type);
+    }
+    else {
+        // identifier
+        string type_name = lexer.consume_expect_token(Token_type::IDENTIFIER).value;
+        return std::make_unique<PathType>(type_name, mut_type, ref_type);
+    }
+}
+
+// 递归下降解析表达式，基于 Pratt 解析器
+// 先考虑掉 loop if while return break continue 这些表达式，然后再使用 Pratt 解析器
+// TO DO !!!
+Expr_ptr Parser::parse_expression(int rbp) {
+
+}
+
+// 处理前缀表达式
+// TO DO !!!
+Expr_ptr Parser::nud(Token token) {
+
+}
+// 处理中缀表达式
+// TO DO !!!
+Expr_ptr Parser::led(Token token, Expr_ptr left) {
+
 }
