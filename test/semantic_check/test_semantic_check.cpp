169975e14f4ff2819ed8174ef470bad04e935638
pigstd <zhuzemu@sjtu.edu.cn>
2025-10-08 22:28:50 +0800

    feat: add semantic_test_tester(not passed)

---
+++ test/semantic_check/test_semantic_check.cpp
@@ -0,0 +1,27 @@
+#include "ast.h"
+#include "semantic_checker.h"
+#include <iostream>
+
+
+/*
+semantic check
+可以通过编译输出 0
+出错输出 -1 并且打印错误信息到 stderr
+*/
+
+int main() {
+    Lexer lexer;
+    try {
+        lexer.read_and_get_tokens();
+        Parser parser(lexer);
+        auto ast = parser.parse();
+        Semantic_Checker checker(ast);
+        checker.checker();
+    } catch(string err_infomation) {
+        std::cerr << err_infomation << std::endl;
+        std::cout << -1 << std::endl;
+        return 0;
+    }
+    std::cout << 0 << std::endl;
+    return 0;
+}
